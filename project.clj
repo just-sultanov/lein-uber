45ef30c1265bc84319c2394afdd3cd685f3d5010
Ilshat Sultanov <just.sultanov@gmail.com>
2021-08-03 13:39:54 +0300

    Initial commit

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject lein-uber "0.0.1-SNAPSHOT"
+  :description "Leiningen plugin that makes an uberjar only when source files are modified after the last build"
+  :url "https://github.com/just-sultanov/lein-uber"
+  :license {:name "Eclipse Public License"
+            :url  "https://www.eclipse.org/legal/epl-v10.html"}
+  :min-lein-version "2.0.0"
+  :eval-in-leiningen true
+  :dependencies [[babashka/fs "0.0.5"]])
