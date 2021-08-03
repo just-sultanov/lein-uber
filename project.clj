2fca66bf4ca549ba53a3f423d316b904395dc01b
Ilshat Sultanov <just.sultanov@gmail.com>
2021-08-03 14:10:24 +0300

    Change group-id

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject lein-uber "0.0.1-SNAPSHOT"
+(defproject team.sultanov/lein-uber "0.0.1-SNAPSHOT"
   :description "Leiningen plugin that makes an uberjar only when source files are modified after the last build"
-  :url "https://github.com/just-sultanov/lein-uber"
+  :url "https://github.com/sultanov-team/lein-uber"
   :license {:name "Eclipse Public License"
             :url  "https://www.eclipse.org/legal/epl-v10.html"}
   :min-lein-version "2.0.0"
